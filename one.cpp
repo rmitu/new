457fee9953e527ff9921acfb5b6baf83e6abb642
rumy14 <miturumy@gmail.com>
2016-08-15 01:15:11 +0600

    asd

---
+++ one.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<ctime>
+#include<iomanip>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+	int randNum1, randNum2;
+	int userTotal, computerTotal;
+
+	int seed = time(0);
+
+	srand(seed);
+	randNum1 = 1 + rand() % 100;
+	randNum2 = 1 + rand() % 100;
+
+	computerTotal = randNum1 + randNum2;
+	cout << setw(5) << randNum1 << endl;
+	cout << setw(3) << "+" << randNum2 << endl;
+	cout << "---\n" ;
+	cin >> userTotal ;
+
+	if( userTotal == computerTotal )
+		cout << "\nGreat Job!\n";
+	else
+		cout  << "Sorry, wrong answer. Correct answer is" << computerTotal <<endl;
+
+	return 0;
+}
